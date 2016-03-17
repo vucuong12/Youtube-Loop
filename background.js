185526ce8e5322584bae66bac71934f51f8d52f2
vucuong12 <colddesert12@gmail.com>
2016-03-18 01:34:38 +0900

    better version

--- background.js
+++ background.js
@@ -1,11 +1,26 @@
 // Called when the user clicks on the icon.
-var currentNumber = 1;
+var currentNumber = 0;
+var bkg = chrome.extension.getBackgroundPage();
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo){
+	if (changeInfo.status === "complete")
+	chrome.browserAction.setBadgeText({ text: currentNumber % 2 === 1? 'On' : 'Off' , tabId: tabId});
+	bkg.console.log(changeInfo.status);
+})
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
+    
     //change icon everytime use clicks on the icon
-    chrome.browserAction.setIcon({path:"icon" + (++currentNumber % 2) + ".png"});
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    ++currentNumber;
+    chrome.browserAction.setIcon({path:"icon0.png"});
+    chrome.browserAction.setBadgeText({ text: currentNumber % 2 === 1? 'On' : 'Off' , tabId: activeTab.id})
+    chrome.tabs.sendMessage(activeTab.id, 
+    	{
+    		"message": "clicked_browser_action", 
+    		"status": currentNumber % 2,
+    		"url": activeTab.url
+    	});
   });
 });
--- content.js
+++ content.js
@@ -1,9 +1,13 @@
 //listen to message from background
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
+		console.log(request);
+		console.log(request.url.indexOf('youtube.com'));
 		if( request.message === "clicked_browser_action" ) {
+			if (request.url.indexOf('youtube.com') == -1) return;
 			var html5Video = $(".video-stream"); // select the video
-			if (html5Video.attr("loop") == "loop")  
+
+			if (request.status == "0")  
 				html5Video.attr("loop", false);   //turn off the loop
 			else
 				html5Video.attr("loop", true);	  //turn on the loop
